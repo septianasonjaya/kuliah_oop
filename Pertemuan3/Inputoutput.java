c29d8d22e74e95d59334a997d8ef52ec60157b1e
septiana sonjaya <32793848+septianasonjaya@users.noreply.github.com>
2017-10-14 18:42:18 +0700

    Add files via upload

---
+++ Pertemuan3/Inputoutput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Inputoutput
+{
+public static void main (String[]args){
+		String data;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Masukan Data : ");
+		data = scan.nextLine();
+		System.out.println("Data yang dimasukan adalah "+data);
+
+	}
+}
+
+
